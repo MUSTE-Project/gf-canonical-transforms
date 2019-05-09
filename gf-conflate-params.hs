ff7d67f2b44948c783dac60216503c90fd421c18
Thomas Hallgren <th-github@altocumulus.org>
2019-05-09 17:04:37 +0200

    Add -d option to change the output directory

--- gf-conflate-params.hs
+++ gf-conflate-params.hs
@@ -9,21 +9,32 @@ import ConflateParams
 import UnqualifyGrammar
 import WriteGrammar
 
-main = transform . parseArgs =<< getArgs
+main = transform =<< parseArgs =<< getArgs
 
-parseArgs args = (eqns,files)
-  where
-    eqns = map (segments (/='=')) eqs
-    (eqs,files) = partition ('=' `elem`) args
+data Args = Args {outdir::FilePath, eqns::[Eqn], files::[FilePath]}
 
-transform (eqns,files@[_]) =
+transform (Args outdir eqns files@[_]) =
   do (utc,(cncname,grammar)) <- GF.batchCompile noOptions files
      let absname = GF.srcAbsName grammar cncname
          canon = GF.grammar2canonical noOptions absname grammar
          canon' = conflateParams eqns (unqualifyGrammar canon)
-     writeGrammar "transformed" canon'
-transform _ =
-  do hPutStrLn stderr usage
+     writeGrammar outdir canon'
+transform _ = abort usage
+
+parseArgs = loop args0
+  where
+    args0 = Args {outdir="transformed", eqns=[], files=[]}
+
+    loop args []                    = return args
+    loop args ["-h"]                = abort usage
+    loop args ["-d"]                = abort "Error: missing dirpath after -d"
+    loop args ("-d":path:ws)        = loop args{outdir=path} ws
+    loop args (w:ws) | '=' `elem` w = loop args{eqns=eqns args++[eqn]} ws
+                     | otherwise    = loop args{files=files args++[w]} ws
+        where eqn = segments (/='=') w
+
+abort message =
+  do hPutStrLn stderr message
      exitFailure
 
-usage = "Usage: gf-conflate-params P=Q[=...] ... ConcreteGrammar.gf"
+usage = "Usage: gf-conflate-params [-d dirpath] P=Q[=...] ... ConcreteGrammar.gf"
